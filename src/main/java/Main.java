e7680a1a3298d31a37e99c5c6d5f326a03059777
sergeevNikita42 <sergeevns95@yandex.ru>
2025-08-15 20:37:11 +0700

    1 изменение

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        String car1, car2, car3;
+        int car1Speed, car2Speed, car3Speed;
+
+
+
+        System.out.println("Введите название машины №1:");
+        car1 = scanner.nextLine();
+        System.out.println("Введите скорость машины №1:");
+        car1Speed = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите название машины №2:");
+        car2 = scanner.nextLine();
+        System.out.println("Введите скорость машины №2:");
+        car2Speed = scanner.nextInt();
+        scanner.nextLine();
+
+
+        System.out.println("Введите название машины №3:");
+        car3 = scanner.nextLine();
+        System.out.println("Введите скорость машины №3:");
+        car3Speed = scanner.nextInt();
+        scanner.nextLine();
+
+
+
+        //После ввода данных пользователем проверяем, что введённая скорость >0 и <=250. Если скорость ввели неверно, программа должна запросить эти данные заново.
+
+        //(speed >0 && speed <=250)
+
+
+        System.out.println("Самая быстрая машина: XXX");
+
+
     }
 }
